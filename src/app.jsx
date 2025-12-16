1f087b69af7c0b44d683993d7f952977353bc641
Arisa Okamoto <qslyu@icloud.com>
2025-12-16 13:42:35 +0900

    注意書きを追加

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className="weather-widget">
       <h1>天気予報オーバーレイ</h1>
-      <p>天気予報は <code>https://weather.tsukumijima.net/</code> から取得しています。（気象庁HPと同じものが取得されます。）</p>
+      <p>本ツールは気象庁のデータを加工したものを<code>https://weather.tsukumijima.net/</code>から取得して表示しています。</p>
+      <p>気象庁ウェブサイトの利用規約に基づき、配信の概要欄等に出典が気象庁である旨を必ず記載してください。表示された情報を元に、ご自身で独自の予報を行うことは、気象業務法で禁じられていますのでご注意ください。</p>
       <h2>利用方法</h2>
       <p>obsのブラウザソースで以下のURLを指定してください</p>
       <code>{window.location.href.replace(/\/$/,'') + '/widget.html'}</code>
