2ab3ce0d07dfc2c9afa4c682de180f9c6c5dcb33
Arisa Okamoto <qslyu@icloud.com>
2025-12-16 13:25:47 +0900

    fix url

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ export default function App() {
       <p>天気予報は <code>https://weather.tsukumijima.net/</code> から取得しています。（気象庁HPと同じものが取得されます。）</p>
       <h2>利用方法</h2>
       <p>obsのブラウザソースで以下のURLを指定してください</p>
-      <code>{window.location.href.replace(/\/$/,'') + '/widget'}</code>
+      <code>{window.location.href.replace(/\/$/,'') + '/widget.html'}</code>
       <br />
       <h2>プレビュー</h2>
       <div style={{containerType: 'inline-size', width:'300px'}}><Widget /></div>
