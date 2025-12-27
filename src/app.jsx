cc5cbad5e908a0cc0bc8a3b99a7dea97abffc41b
Arisa <39028828+qslyu@users.noreply.github.com>
2025-12-28 01:11:26 +0900

    app.jsx を更新

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ export default function App() {
     <div className="weather-widget">
       <h1>obs向け天気予報ウィジェット</h1>
       <p>本ツールは気象庁のデータを加工したものを<code>https://weather.tsukumijima.net/</code>から取得して表示しています。</p>
-      <p>気象庁ウェブサイトの利用規約に基づき、配信の概要欄等に出典が気象庁である旨を必ず記載してください。表示された情報を元に、ご自身で独自の予報を行うことは、気象業務法で禁じられていますのでご注意ください。</p>
+      <p>気象庁ウェブサイトの利用規約に基づき、配信の概要欄等に出典が気象庁である旨を記載してください。</p>
       <h2>利用方法</h2>
       <p>obsのブラウザソースで以下のURLを指定してください</p>
       <code>{window.location.href.replace(/\/$/,'') + '/widget.html'}</code>
